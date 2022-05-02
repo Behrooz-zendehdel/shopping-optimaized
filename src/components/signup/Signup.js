ee07f0427852bef928735771bc06c3cb209ab167
behrooz <behrooz.zendedel1992@gmail.com>
2022-05-02 17:51:22 +0430

    add input page

---
+++ src/components/signup/Signup.js
@@ -0,0 +1,15 @@
+import { useFormik } from "formik";
+
+const signupForm = () => {
+  const formik = useFormik();
+  return (
+    <div>
+      <form>
+
+          
+      </form>
+    </div>
+  );
+};
+
+export default signupForm;
